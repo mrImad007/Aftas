825b20f0c4ebeb9102c84e1fae9776beb567bae7
mrImad007 <imadeddinzaoui@gmail.com>
2023-11-16 18:47:18 +0100

    Starting the project: Project structure

---
+++ src/test/java/com/marjanev2/MarjaneV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marjanev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarjaneV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
